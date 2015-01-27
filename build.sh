870c3fc4f276b281c294a7d3ac1241bb35190a1b
Oskar Holowaty <coding@holowaty.me>
2015-01-27 01:32:32 +0100

    auto testing deployment

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+workdir=`pwd`
+mv lxccu_* old/
 
 start=`date +%s`
 cd src
@@ -5,3 +7,11 @@ yes | debuild -us -uc -d -I.git*
 end=`date +%s`
 runtime=$((end-start))
 echo "Runtime ${runtime} sec."
+
+cd "$workdir"
+
+
+_version=$(cat ./src/debian/changelog | \
+  head -1 | perl -ne '$_=~ /.*\((.*)\).*/; print $1;')
+scp "lxccu_${_version}_all.deb" "lxccu.com:~/lxccu_${_version}_all.deb"
+ssh -t lxccu.com 'sudo mv lxccu_*_all.deb incoming/testing/main && sudo chown -R cdn incoming/testing && sudo su - cdn "./apt_incoming.sh"'
